ab02387ba4916c07d3f0feec5f9cce651e7cfbbf
beauty <wjdqbxl@gachon.ac.kr>
2023-12-21 16:47:57 +0900

    글 삭제(delete) --- - 글 삭제한 후 홈으로 리디렉션(useRouter)

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -1,9 +1,10 @@
 'use client'
 import Link from 'next/link';
-import { useParams } from 'next/navigation'
+import {useParams, useRouter} from 'next/navigation'
 
 export default function Control(){
     const params = useParams();//read페이지의 props를 사용하지 못하므로 useParams 사용
+    const router = useRouter();
     const id = params.id;
 
     return (
@@ -12,7 +13,15 @@ export default function Control(){
                 <li><Link href="/create">Create</Link></li>
                 { id ? <>
                     <li><Link href={`/update/${id}`}>Update</Link></li>
-                    <li><input type="button" value="delete"/></li>
+                    <li><input type="button" value="Delete" onClick={()=>{
+                        const options = {method: "DELETE"}
+                        fetch(`http://localhost:9999/topics/${id}`, options)
+                            .then(resp=>resp.json())
+                            .then(result=>{
+                                router.push('/'); //글 삭제한 후 홈으로 리디렉션
+                                router.refresh();//다시 렌더링하여 화면 출력
+                            });
+                    }}/></li>
                 </> : null }
             </ul>
         </>
