e143348c77efe74b6a8c49ce6c5574e672e4921c
beauty <wjdqbxl@gachon.ac.kr>
2023-12-21 09:43:21 +0900

    글 읽기: 서버 컴포넌트

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -7,10 +7,19 @@ interface ReadProps {
     };
 }
 
-export default function Read(props: ReadProps): JSX.Element {
+interface Topic {
+    id: string;
+    title: string;
+    body: {content : string};
+}
+
+/* 사용자가 글을 읽기만 하는 페이지이므로 상호작용이 일어나지 않음 -> 서버 컴포넌트로 */
+export default async function Read(props: ReadProps): Promise<JSX.Element> {
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const topic = await resp.json();
     return (
         <>
-            <h2>Read Page~</h2>
+            <h2>{topic.title}</h2>
             parameters : {props.params.id}
         </>
     );
