d396d78a3a23b1196a158623db7a07282f40da6d
beauty <wjdqbxl@gachon.ac.kr>
2023-12-21 10:29:48 +0900

    글 생성: useRouter --- - useRouter는 next/navigation(NextJS13)에서 가져옴 - 글을 작성하고 POST 한 글을 가져와서 화면에 뿌리고 생성한 페이지로 리디렉션 - 생성한 페이지로 리디렉션하려면 useRouter API 사용

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,5 +1,41 @@
+"use client"
+import {useRouter} from "next/navigation";
+
 export default function Create() {
+    const router = useRouter();
     return (
-        <>Create Page~</>
+        <>
+            <h2>Create Page~</h2>
+            <form onSubmit={(event)=>{
+                event.preventDefault();
+                const title = event.target.title.value;
+                const body = event.target.body.value;
+                const options = {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({title, "body": {"content": body}})
+                }
+
+                fetch('http://localhost:9999/topics', options)
+                    .then(res=>res.json())
+                    .then(result=>{
+                        console.log(result)
+                        const lastId = result.id;
+                        router.push(`/read/${lastId}`); //생성한 글 페이지로 리디렉션
+                    })
+            }}>
+                <p>
+                    <input type="text" name="title" placeholder="title" />
+                </p>
+                <p>
+                    <textarea name="body" placeholder="body"></textarea>
+                </p>
+                <p>
+                    <input type="submit" value="create"/>
+                </p>
+            </form>
+        </>
     )
 }
